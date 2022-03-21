a9143cf7d9996d1fb25260fbcfef4be763977b76
Rohit Thapar <thaprt206@gmail.com>
2022-03-21 22:49:26 +0530

    changes

--- Assignment 3/euclidian.py
+++ Assignment 3/euclidian.py
@@ -1,14 +1,10 @@
 #!/usr/bin/env python3
 # -- coding: utf-8 --
-"""
-Created on Sun Mar 20 16:48:17 2022
 
-@author: riddhiarora
-"""
 from math import sqrt
 
 
-def riddhi_Manhattan(s, g):
+def Manhattan(s, g):
     total_m = 0
     for i in range(3):
         for j in range(3):
@@ -23,7 +19,7 @@ def riddhi_Manhattan(s, g):
                             y2 = j1
                             total_m += ((abs(x1 - x2)) + (abs(y1 - y2)))
     return total_m
-def riddhi_Eucledian(s, g):
+def Eucledian(s, g):
     total_m = 0
     for i in range(3):
         for j in range(3):
@@ -43,9 +39,9 @@ def riddhi_Eucledian(s, g):
 def main():
     s = [[2, 0, 3], [6, 8, 4], [7, 1, 5]]
     g = [[6, 2, 3], [8, 0, 4], [7, 1, 5]]
-    man = riddhi_Manhattan(s, g)
+    man = Manhattan(s, g)
     print("Manhattan:", man)
-    euc = riddhi_Eucledian(s, g)
+    euc = Eucledian(s, g)
     print("Eucledian:", euc)
 
 
